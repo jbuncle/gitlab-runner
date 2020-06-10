887c5f6d28887028bfe27f54830047f181d6e52f
James Buncle <jbuncle@hotmail.com>
2020-06-10 13:42:39 +0100

    Updated start up checks to have attempt limit, added '/certs/client' to docker-volumes to allow ssh keys per instance

--- start.sh
+++ start.sh
@@ -1,36 +1,31 @@
 #!/bin/bash
 set -e
+set -u
 
 # Default to http protocol for GitLab host server
 : "${GITLAB_PROTOCOL:-http}"
-
-
-if [ "$GITLAB_CONCURRENT_RUNNERS" == "" ] ; then
-    echo "Defaulting GITLAB_CONCURRENT_RUNNERS to 2"
-    GITLAB_CONCURRENT_RUNNERS=2
-fi
-if [ "$GITLAB_LOCAL_HOST" == "" ] ; then
-    echo "Defaulting GITLAB_LOCAL_HOST to $GITLAB_VHOST"
-    GITLAB_LOCAL_HOST=$GITLAB_VHOST
-fi
-
-[ ! -z "$GITLAB_LOCAL_HOST" ] || (echo "Missing GITLAB_LOCAL_HOST" && exit)
+: "${GITLAB_CONCURRENT_RUNNERS:-2}"
 [ ! -z "$GITLAB_REGISTRATION_TOKEN" ] || (echo "Missing GITLAB_REGISTRATION_TOKEN" && exit)
 
 
-# Check if hostname is reachable
-echo "Checking $GITLAB_VHOST is reachable (ping $GITLAB_VHOST)"
-ping -q -c 1 $GITLAB_VHOST > /dev/null  || (echo "Can't reach host $GITLAB_VHOST" && exit)
-        
+counter=1 limit=3
+until ping -q -c 1 $GITLAB_VHOST > /dev/null ; do
+    echo "$GITLAB_VHOST not accessible" 
+    [ "$counter" -lt "$limit" ] || (echo "Reached limit when trying to access ${GITLAB_VHOST}"; exit 1)
+    sleep 3
+    counter=`expr $counter + 1`
+done
+
 echo "Connecting to $GITLAB_PROTOCOL://$GITLAB_VHOST"
 
 # Wait until Gitlab is running
 echo "Fetching status code for $GITLAB_PROTOCOL://$GITLAB_VHOST"
-GITLAB_STATUSCODE="$(curl -L --write-out %{http_code} --silent --output /dev/null $GITLAB_PROTOCOL://$GITLAB_VHOST)"
-while [ "$GITLAB_STATUSCODE" != "302" ]; do
-    echo "Gitlab server not up yet, code is $GITLAB_STATUSCODE, sleeping..."
-    sleep 5
-    GITLAB_STATUSCODE="$(curl --write-out %{http_code} --silent --output /dev/null $GITLAB_PROTOCOL://$GITLAB_VHOST)"
+counter=1 limit=10
+until [ "$(curl -L --write-out %{http_code} --silent --output /dev/null $GITLAB_PROTOCOL://$GITLAB_VHOST)" == 200 ] > /dev/null ; do 
+    [ "$counter" -lt "$limit" ] || (echo "Reached limit waiting for $GITLAB_PROTOCOL://$GITLAB_VHOST to be accessible"; exit 1)
+    echo "Gitlab server not up, code is $GITLAB_STATUSCODE, sleeping..." 
+    sleep 3
+    counter=`expr $counter + 1`
 done
 
 echo "Gitlab server status is $(curl --write-out %{http_code} --silent --output /dev/null $GITLAB_PROTOCOL://$GITLAB_VHOST)"
@@ -39,14 +34,15 @@ echo "Gitlab server status is $(curl --write-out %{http_code} --silent --output 
 RUNNER_COUNT=$(gitlab-runner list 2>&1 | grep Executor | wc -l)
 
 echo "Number of runners registered: $RUNNER_COUNT"
-
 if [[ "$RUNNER_COUNT" == "0" ]] ; then
+    # Generates /etc/gitlab-runner/config.toml
     gitlab-runner register --non-interactive \
      --url "$GITLAB_PROTOCOL://$GITLAB_VHOST/ci/" \
      --registration-token "$GITLAB_REGISTRATION_TOKEN" \
      --executor docker \
      --docker-image gitlab_runner \
      --docker-volumes /var/run/docker.sock:/var/run/docker.sock \
+     --docker-volumes "/certs/client" \
      --docker-privileged
 else 
     echo "Runner previously registered"
